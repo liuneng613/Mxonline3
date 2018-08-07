00abefe0d7b838017176553ab14ee35c8e041f9e
neng.liu <neng.liu@transn.com>
2018-08-07 14:30:13 +0800

    admin中添加管理器&注册。时区,语言,utc(False).数据库中选项参数。female的长度修改

--- apps/users/models.py
+++ apps/users/models.py
@@ -15,7 +15,7 @@ class UserProfile(AbstractUser):
 
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
     birthday = models.CharField(verbose_name=u"生日", null=True, blank=True, max_length=50)
-    gender = models.CharField(max_length=5, verbose_name=u"性别", choices=GENER_CHOICES, default="female")
+    gender = models.CharField(max_length=6, verbose_name=u"性别", choices=GENER_CHOICES, default="female")
     address = models.CharField(max_length=100, verbose_name="地址", default="")
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
