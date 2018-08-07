7522afc93235d7c2fb1457a8e13fb55c95cd929e
neng.liu <neng.liu@transn.com>
2018-08-07 19:47:58 +0800

    Xadmin的安装与源码安装，配置setting中extra_apps

--- Mxonline3/settings.py
+++ Mxonline3/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,6 +44,9 @@ INSTALLED_APPS = [
     'apps.courses',
     'apps.operation',
     'apps.organization',
+    'xadmin',
+    'crispy_forms',
+
 ]
 
 AUTH_USER_MODEL = "users.UserProfile"
@@ -136,3 +142,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
