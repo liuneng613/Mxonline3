6c2a21295f4c885d8767f6e4c4cd0f33d413497c
neng.liu <neng.liu@transn.com>
2018-08-08 10:00:49 +0800

    usersmodels三张表注册进xadmin, 配置搜索过滤展示字段，修复xadmin导出xml错误,导出csv乱码，Unicode重载

---
+++ apps/users/adminx.py
@@ -0,0 +1,27 @@
+__author__ = 'ratel'
+__date__ = '2018/8/7 20:05'
+
+import xadmin
+
+from .models import EmailVerifyRecord
+from .models import Banner
+
+
+class EmailVerifyRecordAdmin(object):
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+# 将model与admin管理器进行关联注册
+xadmin.site.register(Banner, BannerAdmin)
+
